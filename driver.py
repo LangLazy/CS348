5bb96b85767ffff8da43bbbbcc4d9517f6e20658
Rahul Gudise <rahul.gudise@gmail.com>
2023-05-25 16:23:21 -0400

    he works now

--- driver.py
+++ driver.py
@@ -4,11 +4,11 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="rgudise",
   password="ligma",
-  database='users'
+  database='CS348'
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("SELECT * FROM users;")
+mycursor.execute("SELECT * FROM USERS;")
 myresult = mycursor.fetchall()
 for x in myresult:
     print(x)
