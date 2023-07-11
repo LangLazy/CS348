daab418b73af02f76a2d94035a8deb7b419ebf70
Javin <javinmehta52@gmail.com>
2023-07-11 14:56:36 -0400

    going to edit sample, editted readme

--- sample_get_keyword.sql
+++
@@ -1,3 +0,0 @@
-use CS348;
-
-SELECT * FROM paper NATURAL JOIN keywords as t WHERE t.word = 'HVM' Group by paper_id;
--- sample_get_password.sql
+++
@@ -1,3 +0,0 @@
-use CS348;
-
-SELECT u.user_pass FROM user as u WHERE u.email = 'j23mheta@uwaterloo.ca'
